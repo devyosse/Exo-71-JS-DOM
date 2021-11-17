168e5db435c699c6a71dd7fea4902bb60bb387d8
devyosse <narutoshippuden884@gmail.com>
2021-11-17 19:37:22 +0100

    exo 71

--- exercice1.js
+++ exercice1.js
@@ -1,11 +1,18 @@
-let monDiv = .createElement('div');
+let monDiv = document.createElement('div');
 
 document.getElementById('viewport').appendChild(monDiv);
 
+monDiv.innerHTML = 'Ce div a été ajouté en utilisant javascript'
 
+let monDiv2 = document.createElement('div');
 
+monDiv.appendChild(monDiv2);
 
+monDiv2.style.backgroundColor = 'red';
+monDiv2.style.height = '100px';
+monDiv2.style.width = '100px';
 
+let span = document.getElementById('aSupprimer');
 
-
+span.remove(span);
 
